8ac4a3a0c89bb7dd1a01417f7280cc6f2cc11d0e
Farhan21 <farhanabsar21@gmail.com>
2022-02-26 16:11:53 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useEffect, useState } from "react";
+import ReactPaginate from "react-paginate";
+import paginateStyle from './App.module.scss'
 
 function App() {
+
+  const [comments, setComments] = useState([])
+  const [isPending, setIsPending] = useState(false)
+  const [pageLimit, setPageLimit] = useState(0)
+
+  const fetchComments = async () => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_page=1&_limit=12`)
+    const data = await res.json()
+    const totalResponse = res.headers.get('x-total-count')
+    let totalPartialResponse = Math.ceil(totalResponse / 12)
+    setComments(data)
+    setPageLimit(totalPartialResponse)
+  }
+
+  useEffect(() => {
+    fetchComments()
+  }, [])
+
+  const HandlePerPage = useCallback( async (currentPage) => {
+    setIsPending(true)
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_page=${currentPage}&_limit=12`)
+    const data = await res.json()
+    return data
+  }, [])
+
+  const handlePageClick = async (count) => {
+    let newCount = parseInt(count.selected) + 1
+    const fetchPerPage = await HandlePerPage(newCount)
+    setComments(fetchPerPage)
+    setIsPending(false)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={paginateStyle.container}>
+          <h2>Comments</h2>
+          {isPending && <p>Loading..</p>}
+          <div className={paginateStyle.grid}>
+            {comments && comments.map(comment => 
+              <div className={paginateStyle.grid_item} key={comment.id}> 
+                  <h2>{comment.title}</h2>
+                  <p>{comment.body}</p>
+              </div>
+            )}
+          </div>
+      </div>
+
+      <ReactPaginate 
+        previousLabel={''} 
+        previousClassName={'fa-solid fa-angles-left'}
+        nextLabel={''}
+        nextClassName={'fa-solid fa-angles-right'}
+        breakLabel={'...'}
+        pageCount={pageLimit}
+        marginPagesDisplayed={3}
+        onPageChange={handlePageClick}
+        containerClassName={paginateStyle.ul_container}
+        pageClassName={paginateStyle.btn}
+        pageLinkClassName={'link'}
+        activeClassName={paginateStyle.active}
+        disabledClassName={paginateStyle.disabled}
+      />
     </div>
   );
 }
